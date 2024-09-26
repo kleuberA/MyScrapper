ff99d843d4d9403ab107e1e6e914844c9d93df43
Kleuber Silva <kleuber20@hotmail.com>
2024-09-26 17:57:43 -0300

    "Deleted migration files, updated database schema, and removed tables Estado and Concurso."

--- prisma/migrations/20240926203507_l/migration.sql
+++ prisma/migrations/20240926203507_l/migration.sql
@@ -1,18 +1,39 @@
 -- CreateTable
-CREATE TABLE "Law" (
+CREATE TABLE "Estado" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "estado" TEXT NOT NULL,
     "titulo" TEXT NOT NULL,
-    "nomeTitulo" TEXT NOT NULL,
+    "subtitulo" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Concurso" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "orgao" TEXT NOT NULL,
+    "vagas" TEXT NOT NULL,
+    "previsto" BOOLEAN NOT NULL,
+    "estadoId" INTEGER NOT NULL,
+    CONSTRAINT "Concurso_estadoId_fkey" FOREIGN KEY ("estadoId") REFERENCES "Estado" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Law" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "tipo" TEXT NOT NULL,
+    "titulo" TEXT,
+    "nomeTitulo" TEXT,
     "capitulo" TEXT,
     "nomeCapitulo" TEXT,
+    "secao" TEXT,
+    "nomeSecao" TEXT,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "Article" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "lawId" INTEGER NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "lawId" TEXT NOT NULL,
     "artigo" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
@@ -21,8 +42,8 @@ CREATE TABLE "Article" (
 
 -- CreateTable
 CREATE TABLE "Content" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "articleId" INTEGER NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "articleId" TEXT NOT NULL,
     "texto" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
@@ -31,8 +52,8 @@ CREATE TABLE "Content" (
 
 -- CreateTable
 CREATE TABLE "Flashcard" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "articleId" INTEGER NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "articleId" TEXT NOT NULL,
     "front" TEXT NOT NULL,
     "back" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -41,8 +62,18 @@ CREATE TABLE "Flashcard" (
 );
 
 -- CreateTable
+CREATE TABLE "Note" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "articleId" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Note_articleId_fkey" FOREIGN KEY ("articleId") REFERENCES "Article" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
 CREATE TABLE "Question" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "question" TEXT NOT NULL,
     "questionType" TEXT NOT NULL,
     "correctAnswer" TEXT,
@@ -52,8 +83,8 @@ CREATE TABLE "Question" (
 
 -- CreateTable
 CREATE TABLE "Option" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "questionId" INTEGER NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "questionId" TEXT NOT NULL,
     "text" TEXT NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL,
@@ -62,8 +93,8 @@ CREATE TABLE "Option" (
 
 -- CreateTable
 CREATE TABLE "ArticleQuestion" (
-    "articleId" INTEGER NOT NULL,
-    "questionId" INTEGER NOT NULL,
+    "articleId" TEXT NOT NULL,
+    "questionId" TEXT NOT NULL,
 
     PRIMARY KEY ("articleId", "questionId"),
     CONSTRAINT "ArticleQuestion_articleId_fkey" FOREIGN KEY ("articleId") REFERENCES "Article" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
