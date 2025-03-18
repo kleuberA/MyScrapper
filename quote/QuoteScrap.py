00d26bd293c3bbb9c9595025c3ab336c82f5fb61
Kleuber Silva <kleuber20@hotmail.com>
2025-03-18 13:55:06 -0300

    Adicionar suporte à navegação em várias páginas e contagem total de citações no script de extração de citações

--- quote/QuoteScrap.py
+++ quote/QuoteScrap.py
@@ -1,6 +1,10 @@
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.edge.options import Options
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from bs4 import BeautifulSoup
 
 service = Service()
@@ -13,24 +17,44 @@ driver = webdriver.Edge(service=service, options=options)
 def get_quotes():
     driver.get("https://quotes.toscrape.com/")
     driver.implicitly_wait(1)
-    
-    soup = BeautifulSoup(driver.page_source, "html.parser")
     quotes = []
+    
+    while True:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, 'quote'))
+        )
+        
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        element_quote = soup.select("div.col-md-8 > div.quote")
+        
+        for quote in element_quote:
+            text = quote.select_one("span.text").get_text(strip=True)
+            author = quote.select_one("small.author").get_text(strip=True)
+            tags = [tag.get_text(strip=True) for tag in quote.select("a.tag")]
+            quotes.append({
+                "text": text,
+                "author": author,
+                "tags": tags
+            })
+        
+        try:
+            next_button = driver.find_element(By.CSS_SELECTOR, "li.next > a")
+            next_button.click()
+        except NoSuchElementException:
+            break 
+        
+        try:
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.CLASS_NAME, 'quote')))
+        except TimeoutException:
+            break  
+        
 
-    element_quote = soup.select("div.col-md-8 > div.quote")
-    for quote in element_quote:
-        text = quote.select_one("span.text").get_text(strip=True)
-        author = quote.select_one("small.author").get_text(strip=True)
-        tags = [tag.get_text(strip=True) for tag in quote.select("a.tag")]
-        quotes.append({
-            "text": text,
-            "author": author,
-            "tags": tags
-        })
-
+    
     driver.quit()
     return quotes
 
 if __name__ == "__main__":
     quotes = get_quotes()
+    print(f"Total de citações coletadas: {len(quotes)}")
     print(quotes)
